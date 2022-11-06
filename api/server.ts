b3aee14276dd3fa7bea0e62037258ed1896864b0
Ian Erasmus <ian.e@live.co.za>
2022-11-06 16:01:04 +0200

    - Added endpoint to get an adventure by ID

--- api/server.ts
+++ api/server.ts
@@ -27,6 +27,29 @@ app.get('/adventures', (req: any, res: any) => {
             res.send(adventures);
     });
 });
+
+app.get('/adventures/:id', (req: any, res: any) => {
+    fs.readFile(
+        "../data/adventures.json", "utf8", (err: any, jsonString: any) => {
+            if (err) {
+              console.log("File read failed:", err);
+              return;
+            }
+
+            let adventure: AdventureModel;
+            let jsonAdventures = JSON.parse(jsonString);
+
+            let foundAdventure = jsonAdventures.adventure.find((x: any) => x.id === req.params.id);
+
+            if (foundAdventure === undefined) {
+                res.send('Error, not found');
+            }
+
+            adventure = new AdventureModel(foundAdventure.name, foundAdventure.id, foundAdventure.level.start, foundAdventure.level.end);
+
+            res.send(adventure);
+    });
+});
   
 app.listen(5001, function() {
     console.log("Server started on port 5001");
